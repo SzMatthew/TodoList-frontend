acaf27b815a03a11cbce36a902fd1beaeb9aafaa
Máté Szabó <szabomati@gmail.com>
2020-12-06 20:19:52 +0100

    Handle Todos which are done.

--- src/Todo List/Todo/Todo.js
+++ src/Todo List/Todo/Todo.js
@@ -1,14 +1,55 @@
 import React, {Component} from 'react';
 import {BiCheck} from "react-icons/bi";
-import { IconContext } from "react-icons";
+import {IconContext} from "react-icons";
+import classNames from "classnames";
 import './Todo.scss';
 
-class Todo extends Component {
-    render() {
+class Todo extends Component
+{
+    constructor ()
+    {
+        super();
+        this.state = { 
+            prioriy_color: null,
+            priority_color_hex: null
+        }
+    }
+
+    componentDidMount = () =>
+    {
+        switch (this.props.priority)
+        {
+            case 1:
+                this.setState({prioriy_color: 'red'});
+                this.setState({prioriy_color_hex: '#DE4C4A'});
+                break;
+            case 2:
+                this.setState({prioriy_color: 'yellow'});
+                this.setState({prioriy_color_hex: '#FB9814'});
+                break;
+            case 3:
+                this.setState({prioriy_color: 'blue'});
+                this.setState({prioriy_color_hex: '#4271B8'});
+
+                break;
+            default:
+                this.setState({prioriy_color: 'grey'});
+                this.setState({prioriy_color_hex: '#7F7F7F'});
+        }
+    };
+
+    SetDone = (evt) =>
+    { 
+        this.props.onDoneClick(this.props.id)
+    };
+
+    render ()
+    {
+
         return (
             <div className="todo">
-                <div className="priority ">
-                    <IconContext.Provider value={{size: "15px", className: "checked-icon" }}>
+                <div className={classNames('priority', this.state.prioriy_color)} onClick={this.SetDone}>
+                    <IconContext.Provider value={{size: "15px", color: this.state.prioriy_color_hex ,className: "checked-icon" }}>
                         <div>
                             <BiCheck />
                         </div>
--- src/Todo List/Todolist.js
+++ src/Todo List/Todolist.js
@@ -7,17 +7,42 @@ import { IconContext } from "react-icons";
 
 class Todolist extends Component
 {   
+    constructor ()
+    {
+        super();
+        this.state = {
+            'todos': [{id: 0, text: "Wash the dishes", priority: 1, done: false},
+            {id: 1, text: "Clean up the house", priority: 2, done: false},
+            {id: 2, text: "Add new Layer", priority: 3, done: false},
+            {id: 3, text: "Correct all Hard Coded link", priority: 0, done: false}]
+        }
+     }
+
+    HandleTodoDone = (id) =>
+    { 
+        this.setState({
+            todos: this.state.todos.map(todo =>
+            { 
+                if (todo.id === id)
+                {
+                    todo.done = true;
+                }
+                return todo;
+            })
+        });
+
+
+    };
+
     render ()
     {
-        let todos = [{id: 0, text: "Wash the dishes", priority: 1}, {id: 1, text: "Clean up the house", priority: 2}, {id: 2, text: "Add new Layer", priority: 3}];
-
         return (
             <div className="todolist_panel">
                 <div className="todolist-container">
                     <h3 className="project-name">Project Name</h3>
                     {
-                        todos.map(todo => (
-                            <Todo key={todo.id} text={todo.text}/>
+                        this.state.todos.filter(todo => todo.done === false).map( todo => (
+                            <Todo key={todo.id} id={todo.id} text={todo.text} priority={todo.priority} onDoneClick={this.HandleTodoDone}/>
                         ))
                     }
                     <div className="add-task-label">
