1209d41443d659f9a2c031632db4a14351e3163b
SzMatthew <Mate_Szabo1@epam.onmicrosoft.com>
2021-09-23 11:37:58 +0200

    Add basic of TodoList context with reducer pattern

---
+++ src/Contexts/todolist-context.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useMemo, useReducer } from 'react';
+
+const TodoListContext = createContext();
+
+const todoListReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_TODOLIST': {
+            return {todoList: action.payload}
+        }
+        default: { 
+            throw new Error(`Unsupported action type: ${action.type}`);
+        }
+    }
+}
+
+const TodoListProvider = props => {
+    const [state, dispatch] = useReducer(todoListReducer, {todoList: []});
+    const value = useMemo(() => [state, dispatch], [state]);
+    return <TodoListContext.Provider value={value} {...props}/>
+}
+
+const useTodoList = () => {
+    const context = useContext(TodoListContext);
+
+    if (!context) {
+        throw new Error('useTodoList must be used within a TodoListProvider');
+    }
+    const [state, dispatch] = context;
+
+    const setTodoList = todoList => {
+        if (state.todoList !== todoList) {
+            dispatch({type: 'SET_CURRENT_TODOLIST', payload: todoList});
+        } 
+    }
+
+    return {
+        state, 
+        dispatch,
+        setTodoList
+    }
+}
+
+export {useTodoList, TodoListProvider}
